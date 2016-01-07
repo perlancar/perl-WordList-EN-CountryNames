0b89828510c093be2da2abd8471bf6a34b40c897
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-07 23:32:53 +0700

    Initial commit

---
+++ lib/WordList/EN/CountryNames.pm
@@ -0,0 +1,265 @@
+package WordList::EN::CountryNames;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+1;
+# ABSTRACT: English country names
+
+=head1 SEE ALSO
+
+Converted from L<Games::Word::Wordlist::Country> 0.02..
+
+__DATA__
+afghanistan
+aland islands
+albania
+algeria
+american samoa
+andorra
+angola
+anguilla
+antarctica
+antigua and barbuda
+argentina
+armenia
+aruba
+australia
+austria
+azerbaijan
+bahamas
+bahrain
+bangladesh
+barbados
+belarus
+belgium
+belize
+benin
+bermuda
+bhutan
+bolivia
+bonaire, sint eustatius and saba
+bosnia and herzegovina
+botswana
+bouvet island
+brazil
+british indian ocean territory
+brunei
+bulgaria
+burkina faso
+burma
+burundi
+cabo verde
+cambodia
+cameroon
+canada
+cayman islands
+central african republic
+chad
+chile
+china
+christmas island
+cocos islands
+colombia
+comoros
+congo
+congo-kinshasa
+cook islands
+costa rica
+cote d'ivoire
+croatia
+cuba
+curacao
+cyprus
+czech republic
+denmark
+djibouti
+dominica
+dominican republic
+east timor
+ecuador
+egypt
+el salvador
+equatorial guinea
+eritrea
+estonia
+ethiopia
+falkland islands (malvinas)
+faroe islands
+federated states of micronesia
+fiji
+finland
+france
+french guiana
+french polynesia
+french southern territories
+gabon
+gambia
+georgia
+germany
+ghana
+gibraltar
+greece
+greenland
+grenada
+guadeloupe
+guam
+guatemala
+guernsey
+guinea
+guinea-bissau
+guyana
+haiti
+heard island and mcdonald islands
+holy see
+honduras
+hong kong
+hungary
+iceland
+india
+indonesia
+iran
+iraq
+ireland
+isle of man
+israel
+italy
+jamaica
+japan
+jersey
+jordan
+kazakstan
+kenya
+kiribati
+kuwait
+kyrgyzstan
+lao people's democratic republic
+latvia
+lebanon
+lesotho
+liberia
+libya
+liechtenstein
+lithuania
+luxembourg
+macao
+macedonia
+madagascar
+malawi
+malaysia
+maldives
+mali
+malta
+marshall islands
+martinique
+mauritania
+mauritius
+mayotte
+mexico
+monaco
+mongolia
+montenegro
+montserrat
+morocco
+mozambique
+namibia
+nauru
+nepal
+netherlands
+new caledonia
+new zealand
+nicaragua
+niger
+nigeria
+niue
+norfolk island
+north korea
+northern mariana islands
+norway
+oman
+pakistan
+palau
+panama
+papua new guinea
+paraguay
+peru
+philippines
+pitcairn
+poland
+portugal
+puerto rico
+qatar
+republic of moldova
+reunion
+romania
+russian federation
+rwanda
+saint barthelemy
+saint helena
+saint kitts and nevis
+saint lucia
+saint martin
+saint pierre and miquelon
+saint vincent and the grenadines
+samoa
+san marino
+sao tome and principe
+saudi arabia
+senegal
+serbia
+seychelles
+sierra leone
+singapore
+sint maarten (dutch part)
+slovakia
+slovenia
+solomon islands
+somalia
+south africa
+south georgia and the islands
+south korea
+south sudan
+spain
+sri lanka
+state of palestine
+sudan
+suriname
+svalbard and jan mayen
+swaziland
+sweden
+switzerland
+syria
+taiwan
+tajikistan
+thailand
+togo
+tokelau
+tonga
+trinidad and tobago
+tunisia
+turkey
+turkmenistan
+turks and caicos islands
+tuvalu
+uganda
+uk
+ukraine
+united arab emirates
+united republic of tanzania
+united states minor outlying islands
+uruguay
+us
+uzbekistan
+vanuatu
+venezuela, bolivarian republic of
+vietnam
+virgin islands
+virgin islands (uk)
+wallis and futuna
+western sahara
+yemen
+zambia
+zimbabwe
